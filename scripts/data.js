697e2fb9685a183dba3995aaf392d978ef1a440a
Patrick Tamunjoh <38542007+TheMoonrise@users.noreply.github.com>
2020-12-12 21:04:52 +0100

    fixed parts of scatter plot. added missing html elements

--- scripts/data.js
+++ scripts/data.js
@@ -1,11 +1,11 @@
-import { insertCovidData, insertOECDData } from "./dataCleaning.js";
-import { ObservableData } from "./observer.js";
-import { DATA_OBESITY_RAW } from "../data/global-obesity-data.csv.js";
-import { DATA_COVID_RAW } from "../data/global-covid-data.csv.js";
-import { DATA_ALCOHOL_RAW } from "../data/global-alcohol-data.csv.js";
-import { DATA_SMOKING_RAW } from "../data/global-smoking-data.csv.js";
-import { DATA_HEALTHSPENDINGS_RAW } from "../data/global-healthspendings-data.csv.js";
-import { DATA_HOSPITALBEDS_RAW } from "../data/global-hospitalbeds-data.csv.js";
+import { insertCovidData, insertOECDData } from "./dataCleaning.js"
+import { ObservableData } from "./observer.js"
+import { DATA_OBESITY_RAW } from "../data/global-obesity-data.csv.js"
+import { DATA_COVID_RAW } from "../data/global-covid-data.csv.js"
+import { DATA_ALCOHOL_RAW } from "../data/global-alcohol-data.csv.js"
+import { DATA_SMOKING_RAW } from "../data/global-smoking-data.csv.js"
+import { DATA_HEALTHSPENDINGS_RAW } from "../data/global-healthspendings-data.csv.js"
+import { DATA_HOSPITALBEDS_RAW } from "../data/global-hospitalbeds-data.csv.js"
 
 function loadData() {
   //Variables for the current state of the visualization
@@ -14,7 +14,7 @@ function loadData() {
   //To be informed on changes to its value
 
   //The country selected for highlighting and additional info
-  let selectedCountry = new ObservableData("DEU");
+  let selectedCountry = new ObservableData("DEU")
 
   //The factor selected driving the sphere sizes on the main chart and other charts
   const FACTORS = {
@@ -23,45 +23,45 @@ function loadData() {
     ALCOHOL: "alcohol",
     HOSPITALBEDS: "hospitalBeds",
     HEALTHSPENDINGS: "healthSpendings",
-  };
-  let selectedFactor = new ObservableData(FACTORS.SMOKING);
+  }
+  let selectedFactor = new ObservableData(FACTORS.SMOKING)
 
   //The week of the year selected on the timeline
-  let selectedWeek = new ObservableData(6);
+  let selectedWeek = new ObservableData(6)
 
   //The information selected for display on the overview line chart
-  const OVERVIEWS = { INFECTIONS: "infections", DEATHS: "deaths" };
-  let selectedOverview = new ObservableData(OVERVIEWS.INFECTIONS);
+  const OVERVIEWS = { INFECTIONS: "infections", DEATHS: "deaths" }
+  let selectedOverview = new ObservableData(OVERVIEWS.INFECTIONS)
 
   //The countries selected for display
-  const FILTERS = { ALL: "all" };
-  let selectedFilter = new ObservableData(FILTERS.ALL);
+  const FILTERS = { ALL: "all" }
+  let selectedFilter = new ObservableData(FILTERS.ALL)
 
   //Global varaibles to be used by charts and other visualization components
   //Changing these might break other code, so be careful
 
   //Load the core data wrapped as string
-  const DATA_COVID = d3.csvParse(DATA_COVID_RAW());
-  const DATA_SMOKING = d3.csvParse(DATA_SMOKING_RAW());
-  const DATA_ALCOHOL = d3.csvParse(DATA_ALCOHOL_RAW());
-  const DATA_HEALTHSPENDINGS = d3.csvParse(DATA_HEALTHSPENDINGS_RAW());
-  const DATA_HOSPITALBEDS = d3.csvParse(DATA_HOSPITALBEDS_RAW());
-  const DATA_OBESITY = d3.csvParse(DATA_OBESITY_RAW());
+  const DATA_COVID = d3.csvParse(DATA_COVID_RAW())
+  const DATA_SMOKING = d3.csvParse(DATA_SMOKING_RAW())
+  const DATA_ALCOHOL = d3.csvParse(DATA_ALCOHOL_RAW())
+  const DATA_HEALTHSPENDINGS = d3.csvParse(DATA_HEALTHSPENDINGS_RAW())
+  const DATA_HOSPITALBEDS = d3.csvParse(DATA_HOSPITALBEDS_RAW())
+  const DATA_OBESITY = d3.csvParse(DATA_OBESITY_RAW())
 
   //The main data object containing all available data
   //Check dataCleaning.js for the data structure
-  let DATA = {};
+  let DATA = {}
 
   //Use functions from dataCleaning.js to fill the main data structure with raw data from csv files
-  insertCovidData(DATA, DATA_COVID);
-  insertOECDData(DATA, DATA_SMOKING, FACTORS.SMOKING);
-  insertOECDData(DATA, DATA_ALCOHOL, FACTORS.ALCOHOL);
-  insertOECDData(DATA, DATA_HEALTHSPENDINGS, FACTORS.HEALTHSPENDINGS);
-  insertOECDData(DATA, DATA_HOSPITALBEDS, FACTORS.HOSPITALBEDS);
-  insertOECDData(DATA, DATA_OBESITY, FACTORS.OBESITY);
+  insertCovidData(DATA, DATA_COVID)
+  insertOECDData(DATA, DATA_SMOKING, FACTORS.SMOKING)
+  insertOECDData(DATA, DATA_ALCOHOL, FACTORS.ALCOHOL)
+  insertOECDData(DATA, DATA_HEALTHSPENDINGS, FACTORS.HEALTHSPENDINGS)
+  insertOECDData(DATA, DATA_HOSPITALBEDS, FACTORS.HOSPITALBEDS)
+  insertOECDData(DATA, DATA_OBESITY, FACTORS.OBESITY)
 
-  console.log(`data of ${Object.keys(DATA).length} countries and regions found`);
-  console.log(DATA);
+  console.log(`data of ${Object.keys(DATA).length} countries and regions found`)
+  console.log(DATA)
 
   return {
     data: DATA,
@@ -70,7 +70,7 @@ function loadData() {
     selectedFilter: selectedFilter,
     selectedWeek: selectedWeek,
     selectedCountry: selectedCountry,
-  };
+  }
 }
 
-export { loadData };
+export { loadData }
--- scripts/scatterPlot.js
+++ scripts/scatterPlot.js
@@ -1,144 +1,123 @@
 function createScatterPlot(dataContainer) {
+
   // destructure data container
-  const { data, selectedFactor, selectedFilter, selectedWeek, selectedCountry } = dataContainer;
+  const { data, selectedFactor, selectedFilter, selectedWeek, selectedCountry } = dataContainer
 
   //Define some properties for the layout of the plot
-  const scatterPlot = {};
+  const margin = 40
 
-  scatterPlot.margin = 40;
+  const minScale = 8
+  const maxScale = 35
 
-  scatterPlot.minScale = 8;
-  scatterPlot.maxScale = 35;
-
-  scatterPlot.scaleDuration = 800;
-  scatterPlot.moveDuration = 800;
+  const scaleDuration = 800
+  const moveDuration = 800
 
   //Find the maximum infections and deaths
-  scatterPlot.maxCases = Math.max(
+  const maxCases = Math.max(
     ...Object.values(data).map((x) => {
-      return Math.max(...Object.values(x.covid).map((y) => y.cases));
+      return Math.max(...Object.values(x.covid).map((y) => y.cases))
     })
-  );
+  )
 
-  scatterPlot.maxDeaths = Math.max(
+  const maxDeaths = Math.max(
     ...Object.values(data).map((x) => {
-      return Math.max(...Object.values(x.covid).map((y) => y.deaths));
+      return Math.max(...Object.values(x.covid).map((y) => y.deaths))
     })
-  );
+  )
 
   //Grab the svg element and store some properties for convenience
-  scatterPlot.svg = d3.select("#scatterPlot").select(".plot");
-  scatterPlot.width = parseFloat(scatterPlot.svg.style("width")) - 2 * scatterPlot.margin;
-  scatterPlot.height = parseFloat(scatterPlot.svg.style("height")) - 2 * scatterPlot.margin;
+  const svg = d3.select("#scatterPlot").select(".plot")
+  const width = parseFloat(svg.style("width")) - 2 * margin
+  const height = parseFloat(svg.style("height")) - 2 * margin
 
   //Help ourselves to some x axis
-  scatterPlot.x = d3.scaleLinear().domain([0, scatterPlot.maxCases]).range([0, scatterPlot.width]);
-  scatterPlot.svg
-    .append("g")
-    .attr(
-      "transform",
-      "translate(" + scatterPlot.margin + "," + (scatterPlot.height + scatterPlot.margin) + ")"
-    )
-    .call(d3.axisBottom(scatterPlot.x));
+  const x = d3.scaleLinear().domain([0, maxCases]).range([0, width])
+
+  svg.append("g").attr("transform", "translate(" + margin + "," + (height + margin) + ")").call(d3.axisBottom(x))
 
   //Basically the same for the y axis
-  scatterPlot.y = d3
-    .scaleLinear()
-    .domain([0, scatterPlot.maxDeaths])
-    .range([scatterPlot.height, 0]);
-  scatterPlot.svg
-    .append("g")
-    .attr("transform", "translate(" + scatterPlot.margin + "," + scatterPlot.margin + ")")
-    .call(d3.axisLeft(scatterPlot.y));
+  const y = d3.scaleLinear().domain([0, maxDeaths]).range([height, 0])
+  svg.append("g").attr("transform", "translate(" + margin + "," + margin + ")").call(d3.axisLeft(y))
 
   //And a scale axis for convenience
-  scatterPlot.r = d3
-    .scaleLinear()
-    .domain([0, 1])
-    .range([scatterPlot.minScale, scatterPlot.maxScale]);
+  const r = d3.scaleLinear().domain([0, 1]).range([minScale, maxScale])
 
   //Preapare the container hosting the data spheres
-  scatterPlot.container = scatterPlot.svg
-    .append("g")
-    .attr("transform", "translate(" + scatterPlot.margin + "," + scatterPlot.margin + ")");
+  const container = svg.append("g").attr("transform", "translate(" + margin + "," + margin + ")")
 
   //Cleans the data for usage in the scatter plot
-  scatterPlot.scatterPlotData = () => {
-    const data = [];
-    const week = selectedWeek.value;
+  function scatterPlotData() {
+    const data = []
+    const week = selectedWeek.value
 
     Object.keys(data).forEach((x) => {
-      const value = data[x];
+      const value = data[x]
 
-      if (!(selectedFactor.value in value)) return;
-      const factor = value[selectedFactor.value];
+      if (!(selectedFactor.value in value)) return
+      const factor = value[selectedFactor.value]
 
-      if (!(week in value.covid)) return;
-      const covid = value.covid[week];
+      if (!(week in value.covid)) return
+      const covid = value.covid[week]
 
-      data.push({ country: x, cases: covid.cases, deaths: covid.deaths, factor: factor.value });
-    });
+      data.push({ country: x, cases: covid.cases, deaths: covid.deaths, factor: factor.value })
+    })
 
-    return data;
-  };
+    return data
+  }
 
   //Findes the maximum and minimum factors in the data
-  scatterPlot.factorMinMax = (data) => {
-    const min = Math.min(...data.map((x) => x.factor));
-    const max = Math.max(...data.map((x) => x.factor));
-    return { min: min, max: max, span: max - min };
-  };
+  function factorMinMax(data) {
+    const min = Math.min(...data.map((x) => x.factor))
+    const max = Math.max(...data.map((x) => x.factor))
+    return { min: min, max: max, span: max - min }
+  }
 
   //Shows the cleaned data in the plot
-  scatterPlot.showScatterPlot = (data, bounds) => {
-    const update = scatterPlot.container.selectAll("circle").data(data, (x) => x.country);
+  function showScatterPlot(data, bounds) {
+    const update = container.selectAll("circle").data(data, (x) => x.country)
 
     update
       .transition(d3.easeBackInOut)
-      .duration(scatterPlot.moveDuration)
-      .attr("cx", (d) => scatterPlot.x(d.cases))
-      .attr("cy", (d) => scatterPlot.y(d.deaths))
-      .each((d) => {
-        if (d.deaths < 0) console.log(d.country);
-      });
+      .duration(moveDuration)
+      .attr("cx", (d) => x(d.cases))
+      .attr("cy", (d) => y(d.deaths))
+      .each((d) => { if (d.deaths < 0) console.log(d.country) })
 
     update
       .enter()
       .append("circle")
-      .attr("cx", (d) => scatterPlot.x(d.cases))
-      .attr("cy", (d) => scatterPlot.y(d.deaths))
+      .attr("cx", (d) => x(d.cases))
+      .attr("cy", (d) => y(d.deaths))
       .style("fill", (d) => (d.country == "DEU" ? "#ff00ff" : "#ffffff"))
       .transition(d3.easeBackOut)
-      .duration(scatterPlot.scaleDuration)
-      .attrTween("r", (d) =>
-        d3.interpolate(0, scatterPlot.r((d.factor - bounds.min) / bounds.span))
-      );
+      .duration(scaleDuration)
+      .attrTween("r", (d) => d3.interpolate(0, r((d.factor - bounds.min) / bounds.span)))
 
     update
       .exit()
       .transition(d3.easeBackIn)
-      .duration(scatterPlot.scaleDuration)
+      .duration(scaleDuration)
       .attr("r", 0)
-      .remove();
-  };
+      .remove()
+  }
 
   //Updates the scatter plot
-  scatterPlot.updateScatterPlot = () => {
-    const data = scatterPlot.scatterPlotData();
-    const bounds = scatterPlot.factorMinMax(data);
-    scatterPlot.showScatterPlot(data, bounds);
-  };
+  function updateScatterPlot() {
+    const data = scatterPlotData()
+    const bounds = factorMinMax(data)
+    showScatterPlot(data, bounds)
+  }
 
   //Subscribe the update to global events
-  selectedCountry.subscribe(scatterPlot.updateScatterPlot);
-  selectedWeek.subscribe(scatterPlot.updateScatterPlot);
-  selectedFilter.subscribe(scatterPlot.updateScatterPlot);
+  selectedCountry.subscribe(updateScatterPlot)
+  selectedWeek.subscribe(updateScatterPlot)
+  selectedFilter.subscribe(updateScatterPlot)
 
   //Perform the initial update on window load
-  window.addEventListener("load", scatterPlot.updateScatterPlot);
+  window.addEventListener("load", updateScatterPlot)
 
   //TODO remove
-  window.addEventListener("click", (e) => selectedWeek.update(selectedWeek.value + 1));
+  window.addEventListener("click", (e) => selectedWeek.update(selectedWeek.value + 1))
 }
 
-export { createScatterPlot };
+export { createScatterPlot }
