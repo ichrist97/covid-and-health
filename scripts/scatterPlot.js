30f9c8e7c49f8ddfc06a9f26e783ada84d84588f
Patrick Tamunjoh <38542007+TheMoonrise@users.noreply.github.com>
2020-12-17 09:39:07 +0100

    applied theme to scatterplot

--- scripts/scatterPlot.js
+++ scripts/scatterPlot.js
@@ -1,168 +1,191 @@
 /// <reference path='d3.js' />
 
+import { Theme } from './theme.js'
+
 function createScatterPlot(dataContainer) {
-  // destructure data container
-  const { data, selectedFactor, selectedFilter, selectedWeek, selectedCountry } = dataContainer
+	// destructure data container
+	const { data, selectedFactor, selectedFilter, selectedWeek, selectedCountry } = dataContainer
 
-  //Define some properties for the layout of the plot
-  const margin = 40
+	//Define some properties for the layout of the plot
+	const margin = 40
 
-  const minScale = 8
-  const maxScale = 35
+	const minScale = 8
+	const maxScale = 35
 
-  const scaleDuration = 800
-  const moveDuration = 800
+	const scaleDuration = 800
+	const moveDuration = 800
 
-  //Find the maximum infections and deaths
-  const maxCases = Math.max(
-    ...Object.values(data).map(x => {
-      return Math.max(...Object.values(x.covid).map(y => y.cases))
-    })
-  )
+	//Find the maximum infections and deaths
+	const maxCases = Math.max(
+		...Object.values(data).map(x => {
+			return Math.max(...Object.values(x.covid).map(y => y.cases))
+		})
+	)
 
-  const maxDeaths = Math.max(
-    ...Object.values(data).map(x => {
-      return Math.max(...Object.values(x.covid).map(y => y.deaths))
-    })
-  )
+	const maxDeaths = Math.max(
+		...Object.values(data).map(x => {
+			return Math.max(...Object.values(x.covid).map(y => y.deaths))
+		})
+	)
 
-  //Grab the svg element and store some properties for convenience
-  const svg = d3.select('#scatterPlot').select('.plot')
-  const width = parseFloat(svg.style('width')) - 2 * margin
-  const height = parseFloat(svg.style('height')) - 2 * margin
+	//Grab the svg element and store some properties for convenience
+	const svg = d3.select('#scatterPlot').select('.plot')
+	const width = parseFloat(svg.style('width')) - 2 * margin
+	const height = parseFloat(svg.style('height')) - 2 * margin
 
-  //Help ourselves to some x axis
-  const x = d3.scaleLog().domain([1, maxCases]).range([0, width]).clamp(true).nice()
+	//Help ourselves to some x axis
+	const x = d3.scaleLog().domain([1, maxCases]).range([0, width]).clamp(true).nice()
 
-  svg
-    .append('g')
-    .attr('transform', 'translate(' + margin + ',' + (height + margin) + ')')
-    .call(d3.axisBottom(x).ticks(15, '.0f'))
+	const xAxis = svg
+		.append('g')
+		.attr('transform', 'translate(' + margin + ',' + (height + margin) + ')')
+		.call(d3.axisBottom(x).ticks(15, '.0f'))
 
-  //Basically the same for the y axis
-  const y = d3.scaleLog().domain([1, maxDeaths]).range([height, 0]).clamp(true).nice()
+	//Basically the same for the y axis
+	const y = d3.scaleLog().domain([1, maxDeaths]).range([height, 0]).clamp(true).nice()
 
-  svg
-    .append('g')
-    .attr('transform', 'translate(' + margin + ',' + margin + ')')
-    .call(d3.axisLeft(y).ticks(15, '.0f'))
+	const yAxis = svg
+		.append('g')
+		.attr('transform', 'translate(' + margin + ',' + margin + ')')
+		.call(d3.axisLeft(y).ticks(15, '.0f'))
 
-  //And a scale axis for convenience
-  const r = d3.scaleLinear().domain([0, 1]).range([minScale, maxScale])
+	//And a scale axis for convenience
+	const r = d3.scaleLinear().domain([0, 1]).range([minScale, maxScale])
 
-  //Preapare the container hosting the data spheres
-  const container = svg.append('g').attr('transform', 'translate(' + margin + ',' + margin + ')')
+	//Preapare the container hosting the data spheres
+	const container = svg.append('g').attr('transform', 'translate(' + margin + ',' + margin + ')')
 
-  //Cleans the data for usage in the scatter plot
-  function scatterPlotData() {
-    const out = []
-    const week = selectedWeek.value
+	//Cleans the data for usage in the scatter plot
+	function scatterPlotData() {
+		const out = []
+		const week = selectedWeek.value
 
-    Object.keys(data).forEach(x => {
-      const value = data[x]
+		Object.keys(data).forEach(x => {
+			const value = data[x]
 
-      if (!(selectedFactor.value in value)) return
-      const factor = value[selectedFactor.value]
+			if (!(selectedFactor.value in value)) return
+			const factor = value[selectedFactor.value]
 
-      if (!(week in value.covid)) return
-      const covid = value.covid[week]
+			if (!(week in value.covid)) return
+			const covid = value.covid[week]
 
-      out.push({
-        country: x,
-        name: value.name,
-        cases: covid.cases,
-        deaths: covid.deaths,
-        factor: factor.value,
-      })
-    })
+			out.push({
+				country: x,
+				name: value.name,
+				cases: covid.cases,
+				deaths: covid.deaths,
+				factor: factor.value,
+			})
+		})
 
-    return out
-  }
+		return out
+	}
 
-  //Findes the maximum and minimum factors in the data
-  function factorMinMax(data) {
-    const min = Math.min(...data.map(x => x.factor))
-    const max = Math.max(...data.map(x => x.factor))
-    return { min: min, max: max, span: max - min }
-  }
+	//Findes the maximum and minimum factors in the data
+	function factorMinMax(data) {
+		const min = Math.min(...data.map(x => x.factor))
+		const max = Math.max(...data.map(x => x.factor))
+		return { min: min, max: max, span: max - min }
+	}
 
-  //Computes the fill color for a dot from it's factor value
-  function dotFill(value, bounds) {
-    //For now we just blend from black to white
-    const c = (1 - (value - bounds.min) / bounds.span) * 255
-    return `rgb(${c}, ${c}, ${c})`
-  }
+	//Computes the fill color for a dot from it's factor value
+	function dotFill(value, bounds, country = null) {
+		if (country == selectedCountry.value) {
+			return Theme().selection
+		}
 
-  //Compues the sort order for two countries
-  function compareCountries(a, b) {
-    if (a.country == selectedCountry.value) return 1
-    if (b.country == selectedCountry.value) return -1
-    return b.factor - a.factor
-  }
+		const t = 1 - (value - bounds.min) / bounds.span
+		return Theme().primaryBlend(t)
+	}
 
-  //Shows a tooltip for the given country
-  function showTooltip(e, d) {
-    container
-      .append('text')
-      .classed('tooltip', true)
-      .text(d.name)
-      .attr('x', x(d.cases))
-      .attr('y', y(d.deaths))
-      .attr('text-anchor', 'middle')
-      .attr('dominant-baseline', 'central')
-  }
+	//Compues the sort order for two countries
+	function compareCountries(a, b) {
+		if (a.country == selectedCountry.value) return 1
+		if (b.country == selectedCountry.value) return -1
+		return b.factor - a.factor
+	}
 
-  //Removes all tooltips
-  function clearTooltip() {
-    container.selectAll('.tooltip').remove()
-  }
+	//Shows a tooltip for the given country
+	function showTooltip(e, d) {
+		container
+			.append('text')
+			.classed('tooltip', true)
+			.text(d.name)
+			.attr('x', x(d.cases))
+			.attr('y', y(d.deaths))
+			.attr('text-anchor', 'middle')
+			.attr('dominant-baseline', 'central')
+			.attr('fill', Theme().font)
+	}
 
-  //Shows the cleaned data in the plot
-  function showScatterPlot(data, bounds) {
-    const update = container.selectAll('circle').data(data, x => x.country)
+	//Removes all tooltips
+	function clearTooltip() {
+		container.selectAll('.tooltip').remove()
+	}
 
-    update
-      .transition(d3.easeBackInOut)
-      .duration(moveDuration)
-      .attr('r', d => r((d.factor - bounds.min) / bounds.span))
-      .attr('cx', d => x(d.cases))
-      .attr('cy', d => y(d.deaths))
+	//Shows the cleaned data in the plot
+	function showScatterPlot(data, bounds) {
+		const update = container.selectAll('circle').data(data, x => x.country)
 
-    update
-      .enter()
-      .append('circle')
-      .attr('cx', d => x(d.cases))
-      .attr('cy', d => y(d.deaths))
-      .on('mouseenter', showTooltip)
-      .on('mouseleave', clearTooltip)
-      .transition()
-      .duration(scaleDuration)
-      .attrTween('r', d => d3.interpolate(0, r((d.factor - bounds.min) / bounds.span)))
+		update.filter(d => d.country == selectedCountry.value).style('fill', Theme().selection)
 
-    update.exit().transition(d3.easeBackIn).duration(scaleDuration).attr('r', 0).remove()
+		update
+			.transition(d3.easeBackInOut)
+			.duration(moveDuration)
+			.attr('r', d => r((d.factor - bounds.min) / bounds.span))
+			.attr('cx', d => x(d.cases))
+			.attr('cy', d => y(d.deaths))
+			.style('fill', d => dotFill(d.factor, bounds, d.country))
 
-    container
-      .selectAll('circle')
-      .sort(compareCountries)
-      .style('fill', d =>
-        d.country == selectedCountry.value ? '#0055ff' : dotFill(d.factor, bounds)
-      )
-      .on('click', (e, d) => selectedCountry.update(d.country))
-  }
+		const enter = update
+			.enter()
+			.append('circle')
+			.attr('cx', d => x(d.cases))
+			.attr('cy', d => y(d.deaths))
+			.on('mouseenter', (e, d) => {
+				showTooltip(e, d)
+				d3.select(e.currentTarget).style('fill', Theme().hover)
+			})
+			.on('mouseleave', (e, d) => {
+				clearTooltip(e, d)
+				d3.select(e.currentTarget).style('fill', dotFill(d.factor, bounds, d.country))
+			})
+			.style('fill', d => dotFill(d.factor, bounds, d.country))
 
-  //Updates the scatter plot
-  function updateScatterPlot() {
-    const data = scatterPlotData()
-    const bounds = factorMinMax(data)
-    showScatterPlot(data, bounds)
-  }
+		enter
+			.transition()
+			.duration(scaleDuration)
+			.attrTween('r', d => d3.interpolate(0, r((d.factor - bounds.min) / bounds.span)))
 
-  //Subscribe the update to global events
-  selectedCountry.subscribe(updateScatterPlot)
-  selectedWeek.subscribe(updateScatterPlot)
-  selectedFactor.subscribe(updateScatterPlot)
+		update.exit().transition(d3.easeBackIn).duration(scaleDuration).attr('r', 0).remove()
 
-  updateScatterPlot()
+		update
+			.merge(enter)
+			.sort(compareCountries)
+			.on('click', (e, d) => selectedCountry.update(d.country))
+
+		xAxis.selectAll('line').style('stroke', Theme().axis)
+		xAxis.selectAll('path').style('stroke', Theme().axis)
+		xAxis.selectAll('text').style('fill', Theme().axis)
+
+		yAxis.selectAll('line').style('stroke', Theme().axis)
+		yAxis.selectAll('path').style('stroke', Theme().axis)
+		yAxis.selectAll('text').style('fill', Theme().axis)
+	}
+
+	//Updates the scatter plot
+	function updateScatterPlot() {
+		const data = scatterPlotData()
+		const bounds = factorMinMax(data)
+		showScatterPlot(data, bounds)
+	}
+
+	//Subscribe the update to global events
+	selectedCountry.subscribe(updateScatterPlot)
+	selectedWeek.subscribe(updateScatterPlot)
+	selectedFactor.subscribe(updateScatterPlot)
+
+	updateScatterPlot()
 }
 
 export { createScatterPlot }
--- scripts/theme.js
+++ scripts/theme.js
@@ -1,38 +1,41 @@
 //Define the components of a theme
 function DefaultTheme() {
-  //The colors the data is blended between
-  //Primary A is the value for higher factor values
-  this.primaryA = '#00355d'
-  //Primary B is the color for lower factor values
-  this.primaryB = '#deebf7'
-  //The color to display font in which is placed over data elements
-  //Primary Font A is the value for higher factor values
-  this.primaryFontA = '#ffffff'
-  //Primary Font B is the value for lower factor values
-  this.primaryFontB = '#343434'
-  //The color to use for outlines around
-  this.primaryOutline = '#ffffff'
+	//The colors the data is blended between
+	//Primary A is the value for higher factor values
+	this.primaryA = '#00355d'
+	//Primary B is the color for lower factor values
+	this.primaryB = '#deebf7'
+	//A function for blending the primary color
+	this.primaryBlend = t => d3.interpolateRgb(this.primaryA, this.primaryB)(t)
 
-  //The color the currently selected country is displayed in
-  this.selection = '#e6550d'
-  //The color used for hover effect over clickable data displays
-  this.hover = '#fdae6b'
+	//The color to display font in which is placed over data elements
+	//Primary Font A is the value for higher factor values
+	this.primaryFontA = '#ffffff'
+	//Primary Font B is the value for lower factor values
+	this.primaryFontB = '#343434'
+	//The color to use for outlines around
+	this.primaryOutline = '#ffffff'
 
-  //The normal color used for interactable ui elements
-  this.button = '#ababab'
-  //The color used for buttons when hovered over
-  this.buttonHover = '#bcbcbc'
-  //The color used for a currently active ui element
-  this.buttonActive = '#e6550d'
-  //The color to display the font in buttons is
-  this.buttonFont = '#343434'
+	//The color the currently selected country is displayed in
+	this.selection = '#e6550d'
+	//The color used for hover effect over clickable data displays
+	this.hover = '#fdae6b'
 
-  //The color of the graph axis
-  this.axis = '#cdcdcd'
-  //The color of the background of each element of the dashboard
-  this.background = '#232323'
-  //The color to display font in
-  this.font = '#ffffff'
+	//The normal color used for interactable ui elements
+	this.button = '#ababab'
+	//The color used for buttons when hovered over
+	this.buttonHover = '#bcbcbc'
+	//The color used for a currently active ui element
+	this.buttonActive = '#e6550d'
+	//The color to display the font in buttons is
+	this.buttonFont = '#343434'
+
+	//The color of the graph axis
+	this.axis = '#ff0000'
+	//The color of the background of each element of the dashboard
+	this.background = '#232323'
+	//The color to display font in
+	this.font = '#ffffff'
 }
 
 //Define the default theme
@@ -43,12 +46,12 @@ let theme = DEFAULT_THEME
 
 //The function to access the current theme
 function Theme() {
-  return theme
+	return theme
 }
 
 //The function to changes the current theme
 function ChangeTheme(newTheme) {
-  theme = newTheme
+	theme = newTheme
 }
 
 export { Theme, ChangeTheme, DEFAULT_THEME }
