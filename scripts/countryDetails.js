acd023be29ea7396eabeee00145e3eb4ac2e4222
ichrist97 <ivo.christ97@gmail.com>
2020-12-21 18:46:43 +0100

    cumulate until selectedWeek and number format

--- scripts/countryDetails.js
+++ scripts/countryDetails.js
@@ -1,50 +1,59 @@
 import { formatFactor } from './data.js'
 
 function renderCountryDetails(dataContainer) {
-	const { data, selectedFactor, selectedCountry } = dataContainer
+	const { data, selectedFactor, selectedCountry, selectedWeek } = dataContainer
 	setFactor(selectedFactor.value)
-	setDeaths(data, selectedCountry.value)
+	setDeaths(data, selectedCountry.value, selectedWeek.value)
 	setCountryTitle(data, selectedCountry.value)
-	setInfections(data, selectedCountry.value)
+	setInfections(data, selectedCountry.value, selectedWeek.value)
 
 	//subscribe to  observables
 	selectedFactor.subscribe(() => setFactor(selectedFactor.value))
-	selectedCountry.subscribe(() => {
-		setInfections(data, selectedCountry.value)
-		setDeaths(data, selectedCountry.value)
-		setCountryTitle(data, selectedCountry.value)
-	})
+	selectedCountry.subscribe(() => updateCovidData(data, selectedCountry.value, selectedWeek.value))
+	selectedWeek.subscribe(() => updateCovidData(data, selectedCountry.value, selectedWeek.value))
 }
+
+function updateCovidData(data, selectedCountry, selectedWeek) {
+	setInfections(data, selectedCountry, selectedWeek)
+	setDeaths(data, selectedCountry, selectedWeek)
+	setCountryTitle(data, selectedCountry)
+}
+
 function setCountryTitle(data, selectedCountry) {
 	const element = document.querySelector('#selected-country-title')
 	const countryName = data[selectedCountry].name
 	element.innerText = countryName
 }
 
-function setInfections(data, selectedCountry) {
+function setInfections(data, selectedCountry, selectedWeek) {
 	let infections = 0
 	const weeks = Object.values(data[selectedCountry].covid)
 	const scaler = data[selectedCountry].population / 100000
-	weeks.forEach(week => {
+
+	// sum up infections until selectedWeek
+	for (let i = 0; i <= selectedWeek; i++) {
 		// calculate back from population scaler
-		let casesWeek = week.cases * scaler
+		let casesWeek = weeks[i].cases * scaler
 		infections += casesWeek
-	})
+	}
 
 	const element = document.querySelector('#cumulated-cases')
 	const text = formatNumber(infections)
 	element.innerText = text
 }
 
-function setDeaths(data, selectedCountry) {
+function setDeaths(data, selectedCountry, selectedWeek) {
 	let deaths = 0
 	const weeks = Object.values(data[selectedCountry].covid)
 	const scaler = data[selectedCountry].population / 100000
-	weeks.forEach(week => {
+
+	// sum up infections until selectedWeek
+	for (let i = 0; i <= selectedWeek; i++) {
 		// calculate back from population scaler
-		let deathsWeek = week.deaths * scaler
+		let deathsWeek = weeks[i].deaths * scaler
 		deaths += deathsWeek
-	})
+	}
+
 	const element = document.querySelector('#cumulated-deaths')
 	const text = formatNumber(deaths)
 	element.innerText = text
