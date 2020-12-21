756e2d458b923d5b2c9a0acbbfe7177ce93e189c
ichrist97 <ivo.christ97@gmail.com>
2020-12-21 18:35:42 +0100

    cumulate deaths and infections

--- scripts/countryDetails.js
+++ scripts/countryDetails.js
@@ -3,7 +3,9 @@ import { formatFactor } from './data.js'
 function renderCountryDetails(dataContainer) {
 	const { data, selectedFactor, selectedCountry } = dataContainer
 	setFactor(selectedFactor.value)
-	setDeaths(data)
+	setDeaths(data, selectedCountry.value)
+	setCountryTitle(data, selectedCountry.value)
+	setInfections(data, selectedCountry.value)
 
 	//subscribe to  observables
 	selectedFactor.subscribe(() => setFactor(selectedFactor.value))
@@ -20,29 +22,51 @@ function setCountryTitle(data, selectedCountry) {
 }
 
 function setInfections(data, selectedCountry) {
-	const infections = 0
+	let infections = 0
 	const weeks = Object.values(data[selectedCountry].covid)
-	console.log(weeks)
+	const scaler = data[selectedCountry].population / 100000
 	weeks.forEach(week => {
-		// calculate back from population scaler and from per 100.000 capita to absolute value
-		let cases = week.cases * 100000
-		console.log(cases)
+		// calculate back from population scaler
+		let casesWeek = week.cases * scaler
+		infections += casesWeek
 	})
 
 	const element = document.querySelector('#cumulated-cases')
-	element.innerText = infections
+	const text = formatNumber(infections)
+	element.innerText = text
 }
 
-function setDeaths(data) {
+function setDeaths(data, selectedCountry) {
 	let deaths = 0
+	const weeks = Object.values(data[selectedCountry].covid)
+	const scaler = data[selectedCountry].population / 100000
+	weeks.forEach(week => {
+		// calculate back from population scaler
+		let deathsWeek = week.deaths * scaler
+		deaths += deathsWeek
+	})
 	const element = document.querySelector('#cumulated-deaths')
-	element.innerText = deaths
+	const text = formatNumber(deaths)
+	element.innerText = text
 }
 
 function setFactor(value) {
 	const formattedValue = formatFactor(value)
 	const element = document.querySelector('#selected-health-factor')
 	element.innerText = formattedValue
+}
+
+function formatNumber(number) {
+	const str = number.toString()
+	const reverseChars = [...str].reverse()
+	let text = ''
+	for (let i = 0; i < reverseChars.length; i++) {
+		if (i % 3 === 0 && i !== 0 && i !== reverseChars.length) {
+			text = '.' + text
+		}
+		text = reverseChars[i] + text
+	}
+	return text
 }
 
 export { renderCountryDetails }
