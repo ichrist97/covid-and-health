4ab8d12eefc3a3f2eaa91032613cedc049bdd065
Patrick Tamunjoh <38542007+TheMoonrise@users.noreply.github.com>
2021-01-21 15:27:12 +0100

    updated codiv data. corrected/updated description texts

--- scripts/dataCleaning.js
+++ scripts/dataCleaning.js
@@ -14,6 +14,31 @@
 
 //Builds the base data structure from covid case data
 function insertCovidData(data, dataCovid) {
+	dataCovid.forEach(x => {
+		const country = x.countryterritoryCode
+		if (!country) return
+
+		if (!(country in data)) {
+			const name = x.countriesAndTerritories.replaceAll('_', ' ')
+			const values = { name: name, population: x.popData2019, covid: {} }
+			data[country] = values
+		}
+
+		//Only consider the year 2020
+		if (!x.year_week.includes('2020')) return
+
+		//Get the matching week for the data point and insert the value
+		const scaler = data[country].population / 100000
+		const week = parseInt(x.year_week.replaceAll('2020-', ''))
+		data[country].covid[week] = { cases: parseInt(x.cases_weekly) / scaler, deaths: parseInt(x.deaths_weekly) / scaler }
+	})
+}
+
+/*
+Deprecated. This was the function used for the initial csv structure
+Since the csv structure was changed the new version of this function is used
+
+function insertCovidData(data, dataCovid) {
   dataCovid.forEach(x => {
     const country = x.countryterritoryCode
     if (!country) return
@@ -43,91 +68,89 @@ function insertCovidData(data, dataCovid) {
     data[country].covid[week].deaths += parseInt(x.deaths) / scaler
   })
 }
+*/
 
 //Adds data from the OECD csv format to the main data object
 function insertOECDData(data, oecdData, name) {
-  oecdData.forEach(x => {
-    const country = x.LOCATION
+	oecdData.forEach(x => {
+		const country = x.LOCATION
 
-    if (country in data) {
-      const info = { value: x.Value, year: x.TIME }
+		if (country in data) {
+			const info = { value: x.Value, year: x.TIME }
 
-      if (!(name in data[country]) || data[country][name].year < info.year) {
-        data[country][name] = info
-      }
-    } else {
-      console.log(`cannot insert ${name} data for ${country}. country not found.`)
-    }
-  })
+			if (!(name in data[country]) || data[country][name].year < info.year) {
+				data[country][name] = info
+			}
+		} else {
+			console.log(`cannot insert ${name} data for ${country}. country not found.`)
+		}
+	})
 }
 
 //Removes any countries that have data missing
 function removeIncompconsteEntries(data, args) {
-  Object.keys(data)
-    .filter(x => {
-      const value = data[x]
-      if (x == '') return true
+	Object.keys(data)
+		.filter(x => {
+			const value = data[x]
+			if (x == '') return true
 
-      const result = null
-      args.forEach(a => {
-        if (!(a in value)) result = a
-      })
+			const result = null
+			args.forEach(a => {
+				if (!(a in value)) result = a
+			})
 
-      if (result) {
-        console.log(`removed ${value.name}. missing data ${result}`)
-        return true
-      }
+			if (result) {
+				console.log(`removed ${value.name}. missing data ${result}`)
+				return true
+			}
 
-      return false
-    })
-    .forEach(x => delete data[x])
+			return false
+		})
+		.forEach(x => delete data[x])
 }
 
 //I did not feel like conding date stuff myself (maybe the words kind of coding)
 //So this is a solution from stack overflow with only slight modifications
 //https://stackoverflow.com/questions/9045868/javascript-date-getweek/28365677
 Date.prototype.getWeek = function (dowOffset = 1) {
-  var newYear = new Date(this.getFullYear(), 0, 1)
-  var day = newYear.getDay() - dowOffset //the day of week the year begins on
-  day = day >= 0 ? day : day + 7
+	var newYear = new Date(this.getFullYear(), 0, 1)
+	var day = newYear.getDay() - dowOffset //the day of week the year begins on
+	day = day >= 0 ? day : day + 7
 
-  var daynum =
-    Math.floor(
-      (this.getTime() -
-        newYear.getTime() -
-        (this.getTimezoneOffset() - newYear.getTimezoneOffset()) * 60000) /
-        86400000
-    ) + 1
+	var daynum =
+		Math.floor(
+			(this.getTime() - newYear.getTime() - (this.getTimezoneOffset() - newYear.getTimezoneOffset()) * 60000) / 86400000
+		) + 1
 
-  var weeknum
+	var weeknum
 
-  //if the year starts before the middle of a week
-  if (day < 4) {
-    weeknum = Math.floor((daynum + day - 1) / 7) + 1
+	//if the year starts before the middle of a week
+	if (day < 4) {
+		weeknum = Math.floor((daynum + day - 1) / 7) + 1
 
-    if (weeknum > 52) {
-      nYear = new Date(this.getFullYear() + 1, 0, 1)
-      nday = nYear.getDay() - dowOffset
-      nday = nday >= 0 ? nday : nday + 7
+		if (weeknum > 52) {
+			nYear = new Date(this.getFullYear() + 1, 0, 1)
+			nday = nYear.getDay() - dowOffset
+			nday = nday >= 0 ? nday : nday + 7
 
-      /*if the next year starts before the middle of
+			/*if the next year starts before the middle of
               the week, it is week #1 of that year*/
-      weeknum = nday < 4 ? 1 : 53
-    }
-  } else {
-    weeknum = Math.floor((daynum + day - 1) / 7)
-  }
+			weeknum = nday < 4 ? 1 : 53
+		}
+	} else {
+		weeknum = Math.floor((daynum + day - 1) / 7)
+	}
 
-  return weeknum
+	return weeknum
 }
 
 //Converts a date object to the date format used in covid data
 //The applied format is mm/dd/yyyy
 Date.prototype.formated = function formatedDate() {
-  const month = String(this.getMonth() + 1).padStart(2, '0')
-  const year = String(this.getFullYear())
-  const day = String(this.getDate()).padStart(2, '0')
-  return `${day}/${month}/${year}`
+	const month = String(this.getMonth() + 1).padStart(2, '0')
+	const year = String(this.getFullYear())
+	const day = String(this.getDate()).padStart(2, '0')
+	return `${day}/${month}/${year}`
 }
 
 export { insertOECDData, insertCovidData }
